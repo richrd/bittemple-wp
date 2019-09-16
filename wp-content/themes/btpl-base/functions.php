080ad8d989d6760c607579426083eb0153ea727e
Richard. Lewis <richrd.lewis@gmail.com>
2019-09-16 11:34:28 +0000

    Avoid namespace pollution in functions.php

--- wp-content/themes/btpl-base/functions.php
+++ wp-content/themes/btpl-base/functions.php
@@ -1,41 +1,35 @@
 <?php
 
-// TODO: encapsulate into anon class
-class BittempleBaseThemeFunctions {
-    function __construct() {
-        add_action('after_setup_theme', [$this, 'init']);
-    }
+add_action('after_setup_theme', function () {
+    /*
+     * Encapsulate everything in an anonymous class to
+     * avoid polluting the global namespace.
+     */
+    new class() {
+        public function __construct() {
+            // Let WordPress manage the document title tag.
+            add_theme_support('title-tag');
 
-    function init() {
-        // Let WordPress manage the document title tag.
-        add_theme_support('title-tag');
+            // Featured image support
+            add_theme_support('post-thumbnails');
 
-        // Featured image support
-        add_theme_support('post-thumbnails');
+            // Enqueue scripts and styles
+            add_action('wp_enqueue_scripts', [$this, 'enqueue_stylesheets']);
 
-        // Enqueue scripts and styles
-        add_action('wp_enqueue_scripts', [$this, 'enqueue_stylesheets']);
+            // Switch default core markup for search form, comment form, and comments
+            // to output valid HTML5.
+            add_theme_support('html5', array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+            ));
+          }
 
-        // Switch default core markup for search form, comment form, and comments
-        // to output valid HTML5.
-        add_theme_support('html5', array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption',
-        ));
-      }
-
-    function enqueue_stylesheets() {
-        // TODO: possibly use local fontawesome
-        wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.0.7/css/all.css'); 
-    }
-
-    // TODO: Use or remove this
-    function custom_excerpt_length () {
-        return 100;
-    }
-}
-
-$bittemple_base_functions = new BittempleBaseThemeFunctions();
+        public function enqueue_stylesheets() {
+            // TODO: possibly use local fontawesome
+            wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.0.7/css/all.css');
+        }
+    };
+});
