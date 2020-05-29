3fd328d22cb2f298daabeed410f4203c1ae492dd
Richard Lewis <richrd.lewis@gmail.com>
2020-05-29 20:35:42 +0000

    Move wp_footer() to bottom of DOM.

--- wp-content/themes/btpl-base/footer.php
+++ wp-content/themes/btpl-base/footer.php
@@ -1,7 +1,7 @@
-<?php wp_footer(); ?>
         <div id="bottom">
             <div class="content-max-width">
                  <?php get_template_part( "template-parts/footer/footer", "primary" ); ?>
             </div>
         </div>
+<?php wp_footer(); ?>
 <?php get_template_part( "template-parts/body", "end" ); ?>
