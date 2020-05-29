aef847a7acdbaa9dff7ca9b2967e3fcb84daa69c
Richard Lewis <richrd.lewis@gmail.com>
2020-05-29 20:47:44 +0000

    Cleaner header file.

--- wp-content/themes/btpl-base/header.php
+++ wp-content/themes/btpl-base/header.php
@@ -3,21 +3,13 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1">
-    <link rel="shortcut icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.png" />
-    <link rel="favicon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
-
     <?php get_template_part( "template-parts/head-tag"); ?>
-
     <?php wp_head(); ?>
-
 </head>
 
 <?php get_template_part( "template-parts/body", "start" ); ?>
 
         <div id="top">
-            <div class="content-max-width">
-                <?php get_template_part( "template-parts/header/header", "primary" ); ?>
-            </div>
+            <?php get_template_part( "template-parts/header/header", "primary" ); ?>
         </div>
 
